903a1dcd0347b8fbd83860851431f426f4c7986a
chad <chad.palmer@yale.edu>
2020-07-08 15:58:28 -0400

    challenge 1 done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+window.addEventListener('load', function () {
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(function(json) {
+        dogdiv = document.getElementById("dog-image-container")
+        json["message"].forEach(function(img_url) {
+            let img = document.createElement("IMG")
+            img.src = img_url
+            dogdiv.appendChild(img)
+        })
+    })
+})
+
