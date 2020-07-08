5158f84d9576e8bba13bd4b837d69ec76c46a050
larissanguyen <larissa.h.nguyen@gmail.com>
2020-07-08 16:22:09 -0400

    challenge 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 console.log('%c HI', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedJSON = 'https://dog.ceo/api/breeds/list/all'
 
 window.addEventListener('load', function () {
     fetch(imgUrl)
@@ -12,6 +13,25 @@ window.addEventListener('load', function () {
             img.src = img_url
             dogdiv.appendChild(img)
         })
+    })
+})
+
+window.addEventListener('load', function () {
+    fetch(breedJSON)
+    .then(res => res.json())
+    .then(function(json) {
+        breedUL = document.getElementById("dog-breeds")
+        // json["message"].forEach(function(breedJSON) {
+        //     let breed = document.createElement("LI")
+        //     breed.innerText = 
+        //     dogdiv.appendChild(img)
+        // })
+
+        for (var key in json["message"]) {
+            let breed = document.createElement("LI")
+            breed.innerText = key
+            breedUL.appendChild(breed)
+        }
     })
 })
 
